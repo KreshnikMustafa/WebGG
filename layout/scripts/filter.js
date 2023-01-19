169b6a16b758fbc630b997616cd3df453d9ac4a1
Kreshaboii <kreshnikmustafa2005@gmail.com>
2023-01-19 17:00:54 +0100

    useless
    flex ONLY

---
+++ layout/scripts/filter.js
@@ -0,0 +1,16 @@
+var select = document.getElementById("filter-select"); 
+var items = document.getElementsByClassName("item");
+
+select.addEventListener("change", function() {
+    var selectedValue = this.value;
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        if (selectedValue === "all" || item.classList.contains(selectedValue)) {
+            item.classList.remove("hidden");
+        // } else if (selectedValue === "Gjuhë" || item.classList.contains(selectedValue)) {
+        //     item.classList.remove("hidden");
+        }else {
+            item.classList.add("hidden");
+        }
+    }
+});
